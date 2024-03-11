a4b0f8272b1d4a629d21b3166488839c2219afb2
bohdan.snopok <snopokbogdan3@gmail.com>
2024-03-11 15:21:43 +0200

    end main page

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// var swiper = new Swiper(".teams__mySwiper", {
+    //  pagination: {
+       // el: ".swiper-pagination",
+    //    type: "progressbar",
+    //  },
+    //  navigation: {
+    //    nextEl: ".teams__swiper-button-next",
+    //    prevEl: ".teams__swiper-button-prev",
+    //  },
+    //});
