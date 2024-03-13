525ca4c41a86a30cde7d956df2cc5749fc1e157f
bohdan.snopok <snopokbogdan3@gmail.com>
2024-03-13 15:23:50 +0200

    adaptive site end

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,5 +12,6 @@
     document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('burger').addEventListener('click', function () {
             document.querySelector('header').classList.toggle('open')
+            document.querySelector('.header-burger').classList.toggle('burger-position')
         })
     })
